375812dbb97b2303987d161d2124be5d2eb7d1ab
btung07 <brandon.tung25@ycdsbk12.ca>
2024-09-17 10:25:32 -0400

    Completed Java 3

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,1 +1,26 @@
 package gr11review.part1;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Review3 {
+
+    public static void main(String[] args) throws IOException {
+        // Print odd numbers from 20 to 100
+        System.out.println("Odd numbers from 20 to 100:");
+        for (int i = 20; i <= 100; i++) {
+            if (i % 2 != 0) {  // Check if the number is odd
+                System.out.print(i + " ");
+            }
+        }
+       
+        // Seperate the 2 outputs
+        System.out.println();
+       
+        // Print numbers from 29 to 2 in decreasing order
+        System.out.println("Numbers from 29 to 2 in decreasing order:");
+        for (int i = 29; i >= 2; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
